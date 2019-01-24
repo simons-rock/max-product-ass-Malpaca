c0f4c5ad2808ff45401b2ffef283d8e0e16cf5bd
yliu17 <yliu17@simons-rock.edu>
2019-01-24 13:45:27 -0500

    finished

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,13 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int a = arr[0];
+	int b = arr[1];
+	for (int i = 2; i < size; i++){
+		if (arr[i] > b || arr[i] > a){
+			if (a > b) b = arr[i];
+			else a = arr[i];
+		}
+	}
+	return a*b;
 }
-
--- test.c
+++ test.c
@@ -9,25 +9,27 @@ void print_array(int *arr, int size){
 
 void stress_test(int N, int M){
   srand(time(NULL));   // Initialization, should only be called once.
-  
+
   while (1) {
     int n = rand() % (N-2) + 2;      // Returns a pseudo-random integer between 0 and RAND_MAX.
-  
+
     int *arr = malloc(n*sizeof(int));
     for (int i=0; i<n; i++){
-      arr[i] = rand() % M + 1; 
+      arr[i] = rand() % M + 1;
     }
     print_array(arr, n);
-    
+
     int result1 = max_pairwise_product_naive(arr, n);
     int result2 = max_pairwise_product_fast(arr,n);
-    
+    // 
+    // printf("%d\n", result2);
+
     if (result1==result2)
       printf("OK\n");
     else
-      printf("Wrong answer: correct=%d, got instead=%d\n", result1, result2);  
+      printf("Wrong answer: correct=%d, got instead=%d\n", result1, result2);
     free(arr);
-  }  
+  }
 }
 
 int main(int argc, char **argv ){
@@ -35,12 +37,12 @@ int main(int argc, char **argv ){
     printf("To run: test <N> <M>\n");
     return 0;
    }
-   
+
    int N = atoi(argv[1]);
    int M = atoi(argv[2]);
-   
+
    stress_test(N, M);
-   
+
    return 0;
-  
+
 }
